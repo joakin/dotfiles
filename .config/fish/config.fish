e9e6c3b48330fc043f683cd9478de2eea57d2d03
Joakin <joaquin@chimeces.com>
2014-01-03 09:12:26 +0100

    Fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+
+# Add to PATH
+set -g -x PATH /usr/local/bin $PATH
+set -g -x PATH /usr/local/sbin $PATH
+set -g -x PATH /usr/local/share/npm/bin $PATH
+set -g -x PATH ~/.lein/bin $PATH
+set -g -x PATH ~/bin $PATH
+
+alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+alias vi='vim'
+
+# alias to nw
+alias nw="/Applications/node-webkit.app/Contents/MacOS/node-webkit"
+
+alias ls='ls -G'
+alias ll='ls -l'
+alias la='ls -a'
+alias l='ls -lha'
+
+alias rm='rm -i'
+
+alias g='git'
